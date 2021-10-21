bf5d8b44003463b964d8fa575885b1f9f16c9e50
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2021-10-21 20:45:08 +0530

    Update DemoAutomation.java

--- projectname2/src/test/java/projectname/DemoAutomation.java
+++ projectname2/src/test/java/projectname/DemoAutomation.java
@@ -16,3 +16,39 @@ public class DemoAutomation {
 	}
 
 }
+
+
+//email automation
+package gmail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class gmail {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Desktop\\icons folder\\web\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.yahoo.com");
+		WebElement p=driver.findElement(By.xpath("//input[@class='sbq']"));
+		p.sendKeys("Gmail");
+		p.sendKeys(Keys.ENTER);
+		//driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		//driver.findElement(By.xpath("//h3[@class='LC20lb DKV0Md']")).click();
+		//String gmail
+//		driver.findElement(By.xpath("//a[@class='gb_f']")).click();
+		//driver.findElement(By.xpath("/html/body/header/div/div/div/a[2]")).click();
+	//driver.findElement(By.id("identifierId")).sendKeys("saitejaswi.chakravaram05@gmail.com");
+	//driver.findElement(By.xpath("//*[@id='identifierNext']")).click();
+	//driver.findElement(By.xpath("//div[@id='next']")).cli;
+//driver.findElement(By.name("Try again")).click();
+//
+		
+	}
+
+}
+
